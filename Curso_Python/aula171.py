8aecee1aa2c2dd5213462f817fdf5d58b00efc19
MDayvson <mdayvson@outlook.com>
2023-04-19 20:17:47 -0300

    ate aula 175

--- Curso_Python/aula171.py
+++ Curso_Python/aula171.py
@@ -30,3 +30,10 @@ print (lista_soma)
 #     lista_soma.append(lista_a[i] + lista_b[i])
 # print(lista_soma)
 
+
+from itertools import zip_longest
+ 
+lista_a = [10, 2, 3, 4, 5]
+lista_b = [12, 2, 3, 6, 50, 60, 70]
+lista_soma = [x + y for x, y in zip_longest(lista_a, lista_b, fillvalue=0)]
+print(lista_soma)  # [22, 4, 6, 10, 55, 60, 70]
