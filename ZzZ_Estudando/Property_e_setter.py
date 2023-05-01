e51ad85df888e5ed72e184b8a7dcf4de8370e860
MDayvson <mdayvson@outlook.com>
2023-05-01 20:47:21 -0300

    introdução a encapsulamento

---
+++ ZzZ_Estudando/Property_e_setter.py
@@ -0,0 +1,43 @@
+class Pessoa:
+    
+    def __init__(self, nome, idade):
+        self._nome = nome
+        self._idade = idade
+
+        
+    @property                            # Propert usado para retornar um valor
+    def nome(self):
+        print('Estou no property nome')
+        return self._nome
+    
+
+    @property                            # Propert usado para retornar um valor
+    def idade(self):
+        print('Estou no property idade')
+        return self._idade
+
+
+    @nome.setter                          # Setter usado para auterar um valor, não usa return
+    def nome(self, valor=None):
+        print('Entrei no setter nome')
+        self._nome = valor
+
+
+    @idade.setter                         # Setter usado para auterar um valor, não usa return
+    def idade(self, valor=None):
+        print('Entrei no setter idade')
+        self._idade = valor
+
+
+################################################################
+
+p1 = Pessoa('Merari', 35)
+
+print(p1.nome)
+print(p1.idade)
+
+# auterando a variavel p1. usando o setter
+p1.nome = 'Dayvson'
+p1.idade = 32
+
+print(p1.nome, p1.idade)
