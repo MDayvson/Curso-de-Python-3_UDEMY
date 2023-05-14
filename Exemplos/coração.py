9bd77b8f3ae57d483e8f10f45e968729492d167c
MDayvson <mdayvson@outlook.com>
2023-05-13 21:28:30 -0300

    renames

--- Exemplos/coração.py
+++ Exemplos/coração.py
@@ -1,17 +1,19 @@
 # Coração
 
 import math
-from turtle import*
+from turtle import speed, bgcolor, goto, color, done
+
 
 def hearta(k):
-    return  12*math.sin(k)**3
+    return 12*math.sin(k)**3
+
 
 def heartb(k):
-    return 12*math.cos(k)-5*\
-    math.cos(2*k)-2*\
-    math.cos(3*k)-\
-    math.cos(4*k)
-    
+    return 12*math.cos(k)-5 *\
+        math.cos(2*k)-2 *\
+        math.cos(3*k) -\
+        math.cos(4*k)
+
 
 speed(0)
 bgcolor('black')
