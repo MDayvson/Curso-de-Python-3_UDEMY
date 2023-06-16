73bb398d708103aad439c4e71546c6e871f84179
MDayvson <mdayvson@outlook.com>
2023-06-16 03:43:13 -0300

    PySide6

--- AULAS_Conteudo/aula185_requirements.py
+++ AULAS_Conteudo/aula185_requirements.py
@@ -6,3 +6,12 @@
 # pip freeze > requirements.txt
 # Instalando tudo do requirements.txt
 # pip install -r requirements.txt
+
+
+# criar ambiente virtual
+# python -m venv venv
+
+
+# ativar ambiente virtual
+# venv\Scripts\Activate
+# deactivate
